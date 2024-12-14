b15cb38816ffd1e03703e3a5a5b77bf1677a3559
patricio <patoare@hotmail.com>
2024-12-14 18:55:31 +0100

    testing

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import navBar from './assets/images/discordlogo.png'
+import menuburga from './assets/images/menubr.png'
+import backgroundImg from './assets/images/imagenbkgr.png'
+
+
+function App() {
+
+  return (
+      <div className='App'>
+        <h1>hola</h1>
+        <img src={navBar} ></img>
+        <img src={menuburga}></img>
+        <img src={backgroundImg}></img>
+
+      </div>
+  )
+}
+
+export default App
